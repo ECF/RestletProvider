767aa05e44a8d0980c2d9bdbd709f311d452527c
slewis <slewis@composent.com>
2012-04-26 22:21:38 -0700

    Fix for restlet 2.2 snapshot

--- bundles/org.eclipse.ecf.provider.restlet/src/org/eclipse/ecf/internal/provider/restlet/container/RestletClientContainer.java
+++ bundles/org.eclipse.ecf.provider.restlet/src/org/eclipse/ecf/internal/provider/restlet/container/RestletClientContainer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Composent, Inc. and others. All rights reserved. This
+ vbh * Copyright (c) 2011 Composent, Inc. and others. All rights reserved. This
  * program and the accompanying materials are made available under the terms of
  * the Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
@@ -50,7 +50,7 @@ public class RestletClientContainer extends AbstractClientContainer {
 			@SuppressWarnings("rawtypes") Class serviceInterface) {
 		// Introspect the interface for Restlet annotations
 		final List<AnnotationInfo> annotations = org.restlet.engine.resource.AnnotationUtils
-				.getAnnotations(serviceInterface);
+				.getInstance().getAnnotations(serviceInterface);
 		if (annotations == null)
 			return null;
 		List<IRemoteCallable> results = new ArrayList<IRemoteCallable>();
